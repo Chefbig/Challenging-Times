efceabdf6cd3276ce3a28114dfad8648d37ac9d0
Ubuntu <vagrant@Base-Box>
2017-10-29 06:18:05 +0000

    Part one done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  #get 'pages/home'
+
+  #get 'pages', to: 'pages#home', as: '/'
+  get '/', to: 'pages#home', as: 'root'
+
+  get '/about_us', to: 'pages#about', as: 'about'
+
+  #get 'pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
